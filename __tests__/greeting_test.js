a871a86e4d44e3544fb55d8d7d1f3610f7c5e64d
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2023-11-12 20:44:56 +0100

    Handle null, undefined and empty values

--- __tests__/greeting_test.js
+++ __tests__/greeting_test.js
@@ -3,3 +3,14 @@ const greet = require('../greeting');
 test('greet', () => {
     expect(greet('Bob')).toBe('Hello, Bob.')
   })
+
+  test('returns: Hello, my friend given: null', () => {
+    expect(greet(null)).toBe("Hello, my friend.");
+})
+test('returns: Hello, my friend given: undefined', () => {
+    expect(greet(undefined)).toBe("Hello, my friend.");
+})
+test('returns: Hello, my friend given: ""', () => {
+    expect(greet("")).toBe("Hello, my friend.");
+})
+
