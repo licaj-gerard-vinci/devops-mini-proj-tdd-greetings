b7698527179e3e733f7e584a735ae45c75538ca0
Gerard <gerard.licaj@student.vinci.be>
2023-11-16 19:00:38 +0100

    Test fix and formatting code

--- __tests__/greeting_test.js
+++ __tests__/greeting_test.js
@@ -24,17 +24,25 @@ test('returns: Hello, Jill and Jane given: ["Jill", "Jane"]', () => {
 })
 // test5
 test('returns: Hello, Amy, Brian and Charlotte given: ["Amy", "Brian", "Charlotte"]', () => {
-  expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe('Hello, Amy, Brian and Charlotte.')
+  expect(greet(['Amy', 'Brian', 'Charlotte'])).toBe(
+    'Hello, Amy, Brian and Charlotte.'
+  )
 })
 
 test('returns: Hello, Amy, Brian, Dirna and Charlotte given: ["Amy", "Brian", "Charlotte", "Dirna"]', () => {
-  expect(greet(['Amy', 'Brian', 'Dirna', 'Charlotte'])).toBe('Hello, Amy, Brian, Dirna and Charlotte.')
+  expect(greet(['Amy', 'Brian', 'Dirna', 'Charlotte'])).toBe(
+    'Hello, Amy, Brian, Dirna and Charlotte.'
+  )
 })
 // test6
 test('returns: Hello, Amy and Charlotte. AND HELLO BRIAN given: ["Amy", "BRIAN", "Charlotte"]', () => {
-  expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!')
+  expect(greet(['Amy', 'BRIAN', 'Charlotte'])).toBe(
+    'Hello, Amy and Charlotte. AND HELLO BRIAN!'
+  )
 })
 // test7
 test('returns: Bonjour, Amy et Brian given: ["Amy", "Brian", "fr"]', () => {
   expect(greet(['Amy', 'Brian', 'fr'])).toBe('Bonjour, Amy et Brian.')
+  expect(greet(['Amy', 'Brian', 'en'])).toBe('Hello, Amy and Brian.')
+  expect(greet(['Amy', 'Brian', 'nl'])).toBe('Hallo, Amy en Brian.')
 })
